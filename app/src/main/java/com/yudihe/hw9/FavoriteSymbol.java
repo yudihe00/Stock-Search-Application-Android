4780e1324592ad74f9ba1181033239fb0e343033
Yudi He <yudi_he@hotmail.com>
2017-11-22 13:19:27 -0800

    favorite table format done

---
+++ app/src/main/java/com/yudihe/hw9/FavoriteSymbol.java
@@ -0,0 +1,39 @@
+package com.yudihe.hw9;
+
+/**
+ * Created by heyudi on 11/22/17.
+ */
+
+public class FavoriteSymbol {
+    private String symbol;
+    private String price;
+    private String timestamp;
+    private String change;
+    private String changePercent;
+    private String changeString;
+
+    public FavoriteSymbol(String symbol, String price, String timestamp,
+                          String change,String changePercent) {
+        this.symbol = symbol;
+        this.price = price;
+        this.timestamp = timestamp;
+        this.change = change;
+        this.changePercent = changePercent;
+        this.changeString = change+"("+changePercent+")";
+    }
+
+    public String getSymbolName() {
+        return symbol;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getTimeStampt() {
+        return timestamp;
+    }
+    public String getChangeString() {
+        return changeString;
+    }
+}
