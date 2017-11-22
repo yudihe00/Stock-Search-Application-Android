66ae8069abc018e787a880a9d058c75b8bed5345
Yudi He <yudi_he@hotmail.com>
2017-11-22 09:40:28 -0800

    init

--- app/src/main/java/com/yudihe/hw9/GlobalVariables.java
+++ app/src/main/java/com/yudihe/hw9/GlobalVariables.java
@@ -5,6 +5,7 @@ package com.yudihe.hw9;
  */
 
 public class GlobalVariables {
-    public static final String PHP_URL = "http://10.123.161.208/multiAjax.php";
-    public static final String LOCALBASE_URL = "http://10.123.161.208";
+    public static final String LOCALBASE_URL = "http://10.123.96.198";
+    public static final String PHP_URL = LOCALBASE_URL+"/multiAjax.php";
+
 }
