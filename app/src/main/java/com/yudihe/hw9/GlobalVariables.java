23774856e64d37f4d7871f6755cd29ac3c92d246
Yudi He <yudi_he@hotmail.com>
2017-11-22 19:33:59 -0800

    Revert "Merge branch '9-favorite-2'"
    This reverts commit 702803b7bf403d3a18a36f87aa4e3387f1e2b8a6, reversing
    changes made to 4a7af59fad555f45d8b54a1c3fd045c8bc3c0c98.

--- app/src/main/java/com/yudihe/hw9/GlobalVariables.java
+++ app/src/main/java/com/yudihe/hw9/GlobalVariables.java
@@ -5,12 +5,6 @@ package com.yudihe.hw9;
  */
 
 public class GlobalVariables {
-<<<<<<< HEAD
     public static final String PHP_URL = "http://10.123.161.208/multiAjax.php";
     public static final String LOCALBASE_URL = "http://10.123.161.208";
-=======
-    public static final String LOCALBASE_URL = "http://192.168.50.134";
-    public static final String PHP_URL = LOCALBASE_URL+"/multiAjax.php";
-
->>>>>>> 9-favorite-2
 }
