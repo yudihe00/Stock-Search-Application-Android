b00b8c3e6b326da9495c03c51d64e27d4731f43d
Yudi He <yudi_he@hotmail.com>
2017-11-23 07:57:38 -0800

    2 class

--- app/src/main/java/com/yudihe/hw9/FavSingleton.java
+++ app/src/main/java/com/yudihe/hw9/FavSingleton.java
@@ -1,8 +1,39 @@
+// FavSingleton
 package com.yudihe.hw9;
 
+import java.util.ArrayList;
+
 /**
- * Created by heyudi on 11/23/17.
+ * Created by heyudi on 11/22/17.
  */
 
 public class FavSingleton {
+    private static FavSingleton mInstance;
+    private ArrayList<String> list = null;
+
+    public static FavSingleton getInstance() {
+        if(mInstance == null)
+            mInstance = new FavSingleton();
+
+        return mInstance;
+    }
+
+    private FavSingleton() {
+        list = new ArrayList<>();
+    }
+
+    //Add element to array
+    public void addToFav(String value) {
+        list.add(value);
+    }
+
+    // Add symbol into FavList
+    public void deleteFromFav(String value) {
+        list.remove(value);
+    }
+
+    // retrieve array from anywhere
+    public ArrayList<String> getFavList() {
+        return this.list;
+    }
 }
